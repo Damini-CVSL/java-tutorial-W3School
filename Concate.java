444ab7e7f022a8eeef80acea422783b98459813b
Damini-CVSL <126595128+Damini-CVSL@users.noreply.github.com>
2023-02-28 21:39:05 +0530

    Add files via upload

---
+++ Concate.java
@@ -0,0 +1,5 @@
+public class Concate{
+    public static void main(String[] args){
+        System.out.println("Hello! my name is \"Damini\"");
+    }
+}
---
+++ Numbers.java
@@ -0,0 +1,7 @@
+public class Numbers{
+    public static void main(String[] args){
+        /*Create a variable called name of type String and assign it the value "John":*/
+        String a="John";
+        System.out.println(a)
+    }
+}
