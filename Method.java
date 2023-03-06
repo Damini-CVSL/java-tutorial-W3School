e48e7c1a33e9cd2d871e3c50883cd9030be9f194
Damini-CVSL <126595128+Damini-CVSL@users.noreply.github.com>
2023-03-06 10:26:59 -0800

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,8 @@
+public class Method{
+    static void myMethod(String fName){
+        System.out.println(fName +  "Chilakalapudi");
+    }
+    public static void main(String args[]){
+        myMethod("Damini");
+    }
+}
